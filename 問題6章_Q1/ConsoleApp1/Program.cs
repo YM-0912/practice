23c5629d807fcd2a4e4eca8da7b303c66953c3b8
YM-0912 <50792389+YM-0912@users.noreply.github.com>
2019-07-08 00:21:09 +0900

    Add files via upload

---
+++ 問題6章_Q1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    //応用問題：P181-Q1
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //8個の整数
+            int[] numbers = new int[8];
+
+            numbers[0] = 10;
+            numbers[1] = 20;
+            numbers[2] = 30;
+            numbers[3] = 40;
+            numbers[4] = 50;
+            numbers[5] = 60;
+            numbers[6] = 70;
+            numbers[7] = 80;
+
+            //合計値
+            int total = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                total += numbers[i];
+            }
+
+            Console.WriteLine($"合計値は{total}です。");
+
+            //平均値
+            double average = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                average = (double)total / numbers[i];
+            }
+
+            Console.WriteLine($"平均値は{average}です。");
+        }
+    }
+}
---
+++ 問題6章_Q2/問題6章_Q2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 問題6章_Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //①要素数が20の1次元配列に100～119までの整数を順に格納する
+            //②①を格納する際は必ずループ処理を使用する
+            //③全ての要素をcmd.exeに出力する
+
+            //要素数が20の一次元配列
+            int[] numbers = new int[20];
+            //100～119までの整数をnumbersに格納する
+            int num = 99;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                num ++;
+                Console.WriteLine($"要素：{num}");
+            }
+
+        }
+    }
+}
