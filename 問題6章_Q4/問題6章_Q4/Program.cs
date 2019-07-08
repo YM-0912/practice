65123fc25bb6f56b52479156f85361f59c7b9662
YM-0912 <50792389+YM-0912@users.noreply.github.com>
2019-07-09 00:25:14 +0900

    Add files via upload

---
+++ 問題6章_Q4/問題6章_Q4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 問題6章_Q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //6×2の2次元配列
+            //cmd.exeに以下のように表示する 2重ループは使わない
+            //
+            //日曜日(sun)
+            //月曜日(mon)
+            //火曜日(tue)
+            //水曜日(wed)
+            //木曜日(thu)
+            //金曜日(fri)
+            //土曜日(sat)
+
+            //2次元配列の行と列の要素数を指定
+            var oneWeekList = new string[,]
+            {
+                {"日曜日","(sun)" },
+                {"月曜日","(mon)" },
+                {"火曜日","(tue)" },
+                {"水曜日","(wed)" },
+                {"木曜日","(thu)" },
+                {"金曜日","(fri)" },
+                {"土曜日","(sat)" },
+
+            };
+
+            for (int row = 0; row < oneWeekList.GetLength(0);row++)
+            {
+                Console.WriteLine($"{oneWeekList[row,0]}{oneWeekList[row,1]}");
+            }
+
+
+
+        }
+    }
+}
